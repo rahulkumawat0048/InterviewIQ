645b13b2d5def3c8704adf7730812cb69de72ca8
Rahul Kumawat <rahulkumawat0048@gmail.com>
2026-05-02 16:45:08 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use("/api/", limiter);
 
 // Middleware
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://interviewiq-client-er1d.onrender.com",
   credentials: true
 }));
 app.use(express.json());
